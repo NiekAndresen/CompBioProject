c50c20ce84bd7fc1ce2ed4b3228060738a8f2c54
Niek <2andrese@informatik.uni-hamburg.de>
2017-06-01 15:21:03 +0200

    .

--- source/removing_rows.py
+++ source/removing_rows.py
@@ -19,9 +19,6 @@ chunks = pd.read_csv("/home/niek/HSA_data/data_experiment_1_2.csv", usecols=colu
 
 for chunk in chunks:
     chunk[chunk['decoy']==0].to_csv("/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv", mode='a', header=False, index=False)
-    #ex_1_2 total rows: 800k
-    #       decoy == 1: 600k; correlation: 0.0017
-    #       decoy == 0: 200k; correlation: -0.2
 
 #with open("/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv", 'r') as f:
 #    header = f.readline()
