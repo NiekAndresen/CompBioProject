445e2414fd803ed6005e12f5ee7fa296f2304a65
Niek <2andrese@informatik.uni-hamburg.de>
2017-06-01 16:05:53 +0200

    refactored removing rows

--- source/removing_rows.py
+++ source/removing_rows.py
@@ -2,7 +2,11 @@
 import pandas as pd
 import numpy as np
 
-with open('/home/niek/HSA_data/header_reduced', 'r') as hr:
+header_input = '/home/niek/HSA_data/header_reduced'
+input_file = "/home/niek/HSA_data/data_experiment_1_2.csv"
+output_file = "/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv"
+
+with open(header_input, 'r') as hr:
     header = hr.readline()[:-1] #removed \n at the end of line
 columns = []
 for key in header.split(','):
@@ -12,17 +16,13 @@ columns = list(map(lambda string: string.replace('_',' '), columns))
 print(columns)
 
 #write header
-with open("/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv", 'w') as f:
+with open(output_file, 'w') as f:
     f.write(','.join(columns) + '\n')
 
-chunks = pd.read_csv("/home/niek/HSA_data/data_experiment_1_2.csv", usecols=columns, chunksize=1e5)
+chunks = pd.read_csv(input_file, usecols=columns, chunksize=1e5)
 
 for chunk in chunks:
-    chunk[chunk['decoy']==0].to_csv("/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv", mode='a', header=False, index=False)
+    chunk[chunk['decoy']==0].to_csv(output_file, mode='a', header=False, index=False)
+    #todo remove self loops
 
-#with open("/home/niek/HSA_data/data_experiment_1_2_nodecoys.csv", 'r') as f:
-#    header = f.readline()
-#    print(header)
-#    row = f.readline()
-#    print(row)
 
