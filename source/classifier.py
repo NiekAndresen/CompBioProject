241ccce4db64f38c50078d8a5767f134e919a97c
Niek <2andrese@informatik.uni-hamburg.de>
2017-06-20 22:00:24 +0200

    over

--- source/classifier.py
+++ source/classifier.py
@@ -87,14 +87,60 @@ for contact in contacts:
 X = np.concatenate([X[x] for x in X], axis=0)
 nofPositives = int(np.sum(X[:,-1]))
 X = np.concatenate([X[X[:,-1]==1], X[X[:,-1]==0][:nofPositives]], axis=0)
-print(X.shape)
-print(X[:,-1].mean())
+print("training set shape:", X.shape)
+print("proportion of positives in training set:", X[:,-1].mean())
 
-classifier = xval.cv(X[:,:-1], X[:,-1], SVC, {'kernel':['linear']}, nfolds=5, nrepetitions=2, loss_function=xval.zero_one_loss)#xval.false_discovery_rate)
-predictions = classifier.predict(X[:,:-1])
-print('xval loss:', classifier.cvloss)
-print('discoveries:', predictions.sum())
+#crossvalidate and train
+classifier = xval.cv(X[:,:-1], X[:,-1], SVC, {'kernel':['linear', 'rbf']}, nfolds=5, nrepetitions=2, loss_function=xval.zero_one_loss)#xval.false_discovery_rate)
+print('classifier kerneltype:', classifier.kernel)
+print('xval loss (0-1):', classifier.cvloss)
+
+#get a test set
+Xtest = dict()
+chunkCount = 0
+chunks = pd.read_csv(input_fname, usecols=columns, chunksize=1e5)
+for chunk in chunks:
+    chunkCount += 1
+    rows = chunk.sample(n=nofSamplesPerChunk)
+    for i,row in rows.iterrows():
+        try:
+            aa1Idx = int(row['ProteinLink1'])
+            aa2Idx = int(row['ProteinLink2'])
+        except ValueError:
+            continue
+        if aa1Idx > aa2Idx:
+            aa1Idx,aa2Idx = aa2Idx,aa1Idx#smaller index first for contact list
+        if aa1Idx < 5 or aa2Idx < 5: #these AAs are not in the .pdb
+            continue
+        if aa1Idx-4 > native.total_residue() or aa2Idx-4 > native.total_residue():
+            continue
+        pairkey = (aa1Idx, aa2Idx)
+        if not pairkey in Xtest:
+            Xtest[pairkey] = np.zeros(5)[np.newaxis,:]
+        res1pos = native.residue(aa1Idx-4).nbr_atom_xyz()
+        res2pos = native.residue(aa2Idx-4).nbr_atom_xyz()
+        Xtest[pairkey][0,0] += 1#number of contributing rows
+        Xtest[pairkey][0,1] += 1/(row['MatchRank']**2) #rank score
+        Xtest[pairkey][0,2] += row['match score'] #score
+        Xtest[pairkey][0,-1] = res1pos.distance(res2pos) <= 20 #label
+    print("Building test set. Finished chunk number %3d."%chunkCount)
+
+for pairkey in Xtest:
+    Xtest[pairkey][0,2] /= Xtest[pairkey][0,0]
+for contact in contacts:
+    if (contact[0]+4, contact[1]+4) in Xtest:
+        Xtest[(contact[0]+4, contact[1]+4)][0,3] = True #contact
+        
+Xtest = np.concatenate([Xtest[x] for x in Xtest], axis=0)
+
+print("test set shape:", Xtest.shape)
+testPredictions = classifier.predict(Xtest[:,:-1])
+print("proportion of positives in test set:", Xtest[:,-1].mean())
+print("test set discoveries:", testPredictions.sum())
+testNofPosPredicted = testPredictions.sum()
+testNofFalseDiscoveries = testPredictions[np.logical_and(testPredictions==1, Xtest[:,-1]==0)].sum()
+print("test set FDR:", float(testNofFalseDiscoveries)/testNofPosPredicted)
 with open(output_fname, 'w') as f:
     f.write("classifier type: SVM, kernel: %s\n"%(classifier.kernel))
-    f.write("discoveries: %d\n"%int(predictions.sum()))
-    f.write("%f\n"%classifier.cvloss)
+    f.write("test set discoveries: %d\n"%int(testPredictions.sum()))
+    f.write("cvloss: %f\n"%classifier.cvloss)
