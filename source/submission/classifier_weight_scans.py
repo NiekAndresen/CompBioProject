3dfb73f4f9069f558a7df747b7d054a29a306e49
Niek <2andrese@informatik.uni-hamburg.de>
2017-07-30 10:59:11 +0200

    wat

--- source/submission/classifier_weight_scans.py
+++ source/submission/classifier_weight_scans.py
@@ -41,8 +41,8 @@ def valid_idx_pair(idx1, idx2):
         return False
     if idx1-4 > 578 or idx2-4 > 578: #these AAs are not in the .pdb
         return False
-    #if abs(idx1-idx2) < 12: #too low sequence separation
-    #    return False
+    if abs(idx1-idx2) < 12: #too low sequence separation
+        return False
     if idx1 == idx2:
         return False
     return True
@@ -133,61 +133,15 @@ for winner in winners:
     matchesCount += 1
     correctMatchesCount += winner[2]
 
-nofSamplesPerChunk = 500
-#get a test set
-Xtest = dict()
-chunkCount = 0
-chunks = pd.read_csv(input_fname, usecols=columns, chunksize=1e5)
-for chunk in chunks:
-    chunkCount += 1
-    rows = chunk.sample(n=nofSamplesPerChunk)
-    for i,row in rows.iterrows():
-        if row['MatchRank'] > 5:
-            continue
-        aa1List, val1List, pos1List = get_neighborhood_list(row['PeptideLinkMap1'], row['Start1'])
-        aa2List, val2List, pos2List = get_neighborhood_list(row['PeptideLinkMap2'], row['Start2'])
-        for i,aa1Idx in enumerate(pos1List):
-            for j,aa2Idx in enumerate(pos2List):
-                if val1List[i] * val2List[j] < .01: #very unlikely
-                    continue
-                aa1Idx = int(aa1Idx)
-                aa2Idx = int(aa2Idx)
-                if not valid_idx_pair(aa1Idx, aa2Idx):
-                    continue
-                if aa1Idx>aa2Idx:
-                    aa1Idx,aa2Idx = aa2Idx,aa1Idx #smaller index first
-                pairkey = (aa1Idx, aa2Idx)
-                if not pairkey in Xtest:
-                    Xtest[pairkey] = np.zeros(5)[np.newaxis,:]
-                    Xtest[pairkey][0,2] = np.inf #for min()
-                Xtest[pairkey][0,0] += val1List[i] * val2List[j] #weighted number of contributing entries
-                Xtest[pairkey][0,1] += 1/(row['MatchRank']**2) * val1List[i] * val2List[j] #rank score
-                Xtest[pairkey][0,2] = min(Xtest[pairkey][0,1], row['MatchRank']) #minimum rank encoutered
-                Xtest[pairkey][0,3] += row['match score'] * val1List[i] * val2List[j] #weighted score
-                Xtest[pairkey][0,-1] = dist[(aa1Idx-4, aa2Idx-4)] <= 20 #label
-    print("Finished chunk number %3d."%chunkCount)
-
-Xtest = np.concatenate([Xtest[x] for x in Xtest], axis=0)
-
 print('classifier kerneltype:', classifier.kernel)
 print('xval loss (0-1):', classifier.cvloss)
-print("test set shape:", Xtest.shape)
-testPredictions = classifier.predict(Xtest[:,:-1])
-print("proportion of positives in test set:", Xtest[:,-1].mean())
-print("test set discoveries:", testPredictions.sum())
-testNofPosPredicted = testPredictions.sum()
-testNofFalseDiscoveries = testPredictions[Xtest[:,-1]==0].sum()
-print("test set FDR:", float(testNofFalseDiscoveries)/testNofPosPredicted)
 print("precision best winners: %f\n"%(float(correctMatchesCount)/matchesCount))
 print("len(X): %d"%len(X))
 with open(output_fname, 'w') as f:
     f.write("CLASSIFIER WEIGHT SCANS\n")
     f.write("training set shape: %d %d\n"%(Xtrain.shape[0], Xtrain.shape[1]))
     f.write("classifier type: SVM, kernel: %s\n"%(classifier.kernel))
-    f.write("test set shape: %d %d\n"%(Xtest.shape[0], Xtest.shape[1]))
-    f.write("test set discoveries: %d\n"%int(testPredictions.sum()))
     f.write("cvloss: %f\n"%classifier.cvloss)
-    f.write("test precision: %f\n"%(1-float(testNofFalseDiscoveries)/testNofPosPredicted))
     f.write("\nmatches found: %d\n"%matchesCount)
     f.write("precision: %f\n"%(float(correctMatchesCount)/matchesCount))
 
